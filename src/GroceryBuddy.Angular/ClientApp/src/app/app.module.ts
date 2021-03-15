62aa1742fa5ab22e9fc3a473acfc3b0dda54b148
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2021-03-15 16:37:41 +0000

    Added primeng

--- src/GroceryBuddy.Angular/ClientApp/src/app/app.module.ts
+++ src/GroceryBuddy.Angular/ClientApp/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule as ngButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { CounterComponent } from './counter/counter.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
@@ -19,7 +23,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    ngButtonModule,
     AppRoutingModule
   ],
   providers: [],
